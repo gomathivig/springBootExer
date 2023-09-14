efc8a3d1bcea2a51ad8b5df8dfd8d1f09c0f4744
gomathivig <144987803+gomathivig@users.noreply.github.com>
2023-09-14 18:38:30 +1000

    Create repoCheckService.java

---
+++ microservice/src/main/java/com/telstra/codechallenge/repoCheck/repoCheckService.java
@@ -0,0 +1,59 @@
+package com.telstra.codechallenge.repoCheck;
+
+import static com.telstra.codechallenge.utils.DateUtil.getLastLocalDate;
+import static com.telstra.codechallenge.utils.DateUtil.getYYYYMMDD;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+@Slf4j
+public class repoCheckService {
+
+  public static final String SORT_KEY = "sort";
+  public static final String QUERY = "q";
+  public static final String ORDER_KEY = "order";
+  public static final String PER_PAGE_LIMIT = "per_page";
+  public static final String DESC = "desc";
+  public static final String STARS = "stars";
+  public static final String CREATED_DATE = "created:";
+
+  private final RestTemplate restTemplate;
+
+  @Value("${git.base.url}")
+  private String gitBaseUrl;
+
+  @Autowired
+  public repoCheckService(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+
+
+  public repoCheckResponse getHottestRepos(int noOfRepos, int lastDays) {
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+
+    UriComponentsBuilder builder = getUriComponentsBuilder(gitBaseUrl, noOfRepos, lastDays);
+
+    return this.restTemplate.exchange(
+        builder.build().toString(), HttpMethod.GET, new HttpEntity<>(headers),
+        repoCheckResponse.class).getBody();
+  }
+
+  private UriComponentsBuilder getUriComponentsBuilder(String url, int noOfRepos, int lastDays) {
+    return UriComponentsBuilder.fromHttpUrl(url)
+          .queryParam(QUERY, CREATED_DATE + getYYYYMMDD(getLastLocalDate(lastDays)))
+          .queryParam(SORT_KEY, STARS)
+          .queryParam(ORDER_KEY, DESC)
+          .queryParam(PER_PAGE_LIMIT, noOfRepos);
+  }
+}
